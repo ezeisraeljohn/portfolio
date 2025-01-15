7cab1e67d4cf4f2d7fd8e381e7d64a072d4c6e33
ezeisraeljohn <ezeisraeljohn@gmail.com>
2025-01-15 14:16:27 +0100

    Add Resume component with ResumeCard for job experience and education sections

---
+++ app/ui/resume.tsx
@@ -0,0 +1,59 @@
+function ResumeCard({
+  title,
+  place,
+  yearRange,
+  description,
+}: {
+  title: String;
+  place: String;
+  yearRange: String;
+  description: String;
+}) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <h4>{`${place}(${yearRange})`}</h4>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function ResumeType() {
+  return (
+    <div>
+      <div>
+        <h3>Job Experience</h3>
+        <p>2020-2024</p>
+        <ResumeCard
+          title="Backend Developer"
+          place="InternPulse"
+          yearRange="2024"
+          description="Collaborated with a team of developers to build a scalable and secure RESTful API using Node.js, Express, Python Django, and Postgres"
+        ></ResumeCard>
+      </div>
+
+      <div>
+        <h3>Education</h3>
+        <p>2016-2020</p>
+        <ResumeCard
+          title="BSc. Computer Science"
+          place="University of Nigeria"
+          yearRange="2016-2020"
+          description="Graduated with a 2:1 in Computer Science"
+        ></ResumeCard>
+      </div>
+    </div>
+  );
+}
+
+export function Resume() {
+  return (
+    <>
+      <section id="resume">
+        <h3>RESUME</h3>
+        <h2>My Resume</h2>
+        <ResumeType></ResumeType>
+      </section>
+    </>
+  );
+}
